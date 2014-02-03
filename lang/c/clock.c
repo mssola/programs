4ec4920d6a2d2d3bf6cc651676e4006c98318d9d
Miquel Sabaté <mikisabate@gmail.com>
2014-02-03 20:32:41 +0100

    c: added the clock.c program. Ooh.. nostalgia :)

---
+++ lang/c/clock.c
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2014 Miquel Sabaté Solà <mikisabate@gmail.com>
+ */
+
+/*
+ * This file is not that interesting. It's more a rendition of a program
+ * that I did in the EC1 course.
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+void wait(int seconds)
+{
+    clock_t endwait;
+
+    endwait = clock() + seconds * CLOCKS_PER_SEC;
+    while (clock() < endwait);
+}
+
+int main(int argc, char *argv[])
+{
+    int n, sec;
+    char *a = "Aquest programa funciona";
+
+    sec = (argc == 2) ? atoi(argv[1]) : 1;
+    for (; *a; ++a) {
+        wait(sec);
+        printf("%c",*a);
+        fflush(stdout);
+    }
+    printf("\n");
+    return 0;
+}
