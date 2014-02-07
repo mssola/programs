4e9b4788dd0f0d413aa818edb843e96ab1f91107
Miquel Sabaté <mikisabate@gmail.com>
2014-02-07 07:41:40 +0100

    go: added a file showing decorators in Go.

---
+++ lang/go/misc/decorators.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+    "fmt"
+)
+
+type Func func(i int) int
+
+// Simple decorator that multiplies the result by 4 if the original value was
+// lesser than 4.
+func decorator(f Func) Func {
+    return func(i int) int {
+        res := f(i)
+        if i < 4 {
+            return res * 4
+        }
+        return res;
+    }
+}
+
+func original(i int) int {
+    return i
+}
+
+func main() {
+    decorated := decorator(original)
+
+    fmt.Println(decorated(2)) // => 2 * 4
+    fmt.Println(decorated(5)) // => 5
+}
+
