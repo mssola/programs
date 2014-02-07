5b714b920b9c2a8c50c31853ce9332a2f56b71a3
Miquel Sabaté <mikisabate@gmail.com>
2014-02-07 07:26:28 +0100

    go: added an example showing concurrency through replicas.

---
+++ lang/go/concurrency/google.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+    "time"
+    "fmt"
+    "math/rand"
+)
+
+var (
+    Web1 = fakeSearch("web1")
+    Web2 = fakeSearch("web2")
+    Image1 = fakeSearch("image1")
+    Image2 = fakeSearch("image2")
+    Video1 = fakeSearch("video1")
+    Video2 = fakeSearch("video2")
+)
+
+type Result string
+type Search func(query string) Result
+
+// Returns a Search function. This function will wait for a random number
+// of milliseconds and it will finally return a string with the result.
+func fakeSearch(kind string) Search {
+    return func(query string) Result {
+        time.Sleep(time.Duration(rand.Intn(100)) * time.Millisecond)
+        return Result(fmt.Sprintf("%s result for %q\n", kind, query))
+    }
+}
+
+// We don't want to wait for a specific replica. We'll use the result from
+// the replica that gets the result first.
+func First(query string, replicas ...Search) Result {
+    c := make(chan Result)
+    searchReplica := func(i int) { c <- replicas[i](query) }
+    for i := range replicas {
+        go searchReplica(i)
+    }
+    return <-c
+}
+
+// Perform the given query to web, images and videos.
+func Google(query string) (results []Result) {
+    c := make(chan Result)
+    go func() { c <-First(query, Web1, Web2) }()
+    go func() { c <-First(query, Image1, Image2) }()
+    go func() { c <-First(query, Video1, Video2) }()
+
+    timeout := time.After(80 * time.Millisecond)
+    for i := 0; i < 3; i++ {
+        select {
+        case result := <-c:
+            results = append(results, result)
+        case <-timeout:
+            fmt.Println("timed out")
+            return
+        }
+    }
+    return
+}
+
+func main() {
+    rand.Seed(time.Now().UnixNano())
+    start := time.Now()
+    results := Google("golang")
+    elapsed := time.Since(start)
+    fmt.Println(results)
+    fmt.Println(elapsed)
+}
+
