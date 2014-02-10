e3b68f5705b6a5fbd5a6e71f3a08dc23b4ab587e
Miquel Sabaté <mikisabate@gmail.com>
2014-02-10 12:47:32 +0100

    go: added an example of JSON-encoded data.

---
+++ lang/go/misc/json.go
@@ -0,0 +1,36 @@
+// Copyright 2014 Miquel Sabaté <mikisabate@gmail.com>
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type Configuration struct {
+	Database map[string]struct {
+		Dbname   string
+		Host     string
+		User     string
+		Password string
+	}
+
+	Redis struct {
+		Socket string
+		Db     string
+	}
+}
+
+func main() {
+	file, _ := os.Open("data/config.json")
+	decoder := json.NewDecoder(file)
+	c := &Configuration{}
+	decoder.Decode(&c)
+
+	// Database
+	fmt.Printf("Dbname of test: %v\n", c.Database["Test"].Dbname)
+
+	// Redis
+	fmt.Printf("Redis. Socket: %v ; DB: %v\n", c.Redis.Socket, c.Redis.Db)
+}
