894ba8b48b1da596ae0f4b8fc73a7e37c0e86846
Miquel Sabaté <mikisabate@gmail.com>
2014-02-14 22:28:51 +0100

    os: added a file showing syscalls related to I/O.

---
+++ os/io.c
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2014 Miquel Sabaté Solà <mikisabate@gmail.com>
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>
+#include <err.h>
+
+
+int main(int argc, char *argv[])
+{
+    int count;
+    int fd, fd1;
+    char buffer[256];
+
+    /*
+     * Setting the umask value here. You can check the default value
+     * at /etc/profile.
+     */
+    umask(0);
+
+    /*
+     * Opening this guys to do the reading & writing.
+     */
+    if ((fd = open("io.c", O_RDWR, 0)) < 0)
+        err(1, "open");
+    if ((fd1 = open("/tmp/thing.c", O_TRUNC | O_WRONLY | O_CREAT, 0666)) < 0)
+        err(1, "open");
+
+    /* Buffering: just doing a simple copy. */
+    while ((count = read(fd, buffer, 256)) > 0)
+        write(fd1, buffer, count);
+
+    /* For some reason we want to print the first character of this file. */
+    if (lseek(fd, 0, SEEK_SET) < 0)
+        err(1, "lseek");
+    if (read(fd, buffer, 1) != 1)
+        err(1, "read");
+
+    /* Let's use the dup syscall, just to show how it operates. */
+    count = dup(1);
+    sprintf(buffer, "The first character is: %c.\n", buffer[0]);
+    write(count, buffer, strlen(buffer));
+
+    /* Let's close everything and return a success status. */
+    close(fd);
+    close(fd1);
+    close(count);
+    return (EXIT_SUCCESS);
+}
