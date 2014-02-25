4445f5d44c47b4700674b4edaaa6d800eca53d79
Miquel Sabaté <mikisabate@gmail.com>
2014-02-25 08:20:59 +0100

    algorithms: added the simple trick of determining power of two numbers.

---
+++ algorithms/misc/power_of_two.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+
+int power_of_two(unsigned int a)
+{
+    if (a < 2)
+        return 0;
+    return (a & (a - 1)) == 0;
+}
+
+int main(int argc, char *argv[])
+{
+    printf("1 power of two: %d\n", power_of_two(1));
+    printf("8 power of two: %d\n", power_of_two(8));
+    printf("9 power of two: %d\n", power_of_two(9));
+}
+
