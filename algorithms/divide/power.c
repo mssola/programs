c9076d637c48bc83ef123f5abda6220d7d1916d7
Miquel Sabaté <mikisabate@gmail.com>
2014-02-25 08:13:20 +0100

    algorithms: added the power math function that is using a divide and conquer.

---
+++ algorithms/divide/power.c
@@ -0,0 +1,22 @@
+
+#include <stdio.h>
+
+/*
+ * Computing a**n with a divide and conquer algorithm.
+ */
+int power(unsigned int a, unsigned int n)
+{
+    if (n == 0)
+        return 1;
+
+    if ((n % 2) == 0)
+        return power(a*a, n/2);
+    return a * power(a*a, n/2);
+}
+
+int main()
+{
+    printf("%d\n", power(2, 4));
+    printf("%d\n", power(3, 3));
+}
+
