9db74ad4b9874065363b60c47a46280c678c7c79
Miquel Sabaté <mikisabate@gmail.com>
2014-12-09 10:20:33 +0100

    algorithms/misc: fix the style.

--- algorithms/misc/power_of_two.c
+++ algorithms/misc/power_of_two.c
@@ -8,16 +8,16 @@
 
 int power_of_two(unsigned int a)
 {
-    if (a < 2) {
-        return 0;
+	if (a < 2) {
+		return 0;
 	}
-    return (a & (a - 1)) == 0;
+	return (a & (a - 1)) == 0;
 }
 
 int main(int argc, char *argv[])
 {
-    printf("1 power of two: %d\n", power_of_two(1));
-    printf("8 power of two: %d\n", power_of_two(8));
-    printf("9 power of two: %d\n", power_of_two(9));
+	printf("1 power of two: %d\n", power_of_two(1));
+	printf("8 power of two: %d\n", power_of_two(8));
+	printf("9 power of two: %d\n", power_of_two(9));
 }
 
